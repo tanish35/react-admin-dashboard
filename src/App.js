4edd79b147e2e0cc505782ae79d56abe4b4cf0e6
tanish35 <142332924+tanish35@users.noreply.github.com>
2024-05-22 10:50:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Bar from "./scenes/bar";
 import Form from "./scenes/form";
-import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 import Geography from "./scenes/geography";
